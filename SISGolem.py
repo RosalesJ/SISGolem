619dbb5ee4ac0014d188b4c602e6a61f29b11865
Jacob Rosales Chase <coby@case.edu>
2017-03-10 23:57:17 -0500

    Add brief documentation to each method

--- SISGolem.py
+++ SISGolem.py
@@ -12,21 +12,24 @@ from collections import OrderedDict
 classes_file = '/Users/cobyrosales/Documents/School/Classes.csv'
 login_page =  'https://sismobile.case.edu/app/profile/logintoapp'
 search_page = "https://sis.case.edu/psc/P90SCWR_1/EMPLOYEE/P90SCWR/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL"
-authentication = ('CaseID', 'Password')
+authentication = ('Username', 'Password')
 
 #[Status, Name, Title, Catalog Number, Times, Room, Instructor, Dates, Cap, Enrolled]
-disp_arr = [6, 9, 20, 0, 20, 0, 10, 0, 0, 0]
-seconds_between_requests = 20s
+display_array = [6, 9, 20, 0, 20, 0, 10, 0, 0, 0]
+seconds_between_requests = 20
 
 def log(event):
+    '''Logs a string event, currently prints to command line'''
     print(str(datetime.now().strftime('%H:%M:%S')) + ' ::: ' + str(event))
 
 
 def logged_in(page_response):
+    '''Checks if logged in given a page response'''
     return "ERP Student Information System Sign-in" not in page_response
 
 
 def login(session, auth):
+    '''Logs session into SIS'''
     log('Logging into SIS')
     payload = {
         'username': auth[0],
@@ -48,7 +51,12 @@ def login(session, auth):
     return True
 
 
-def get_classes(session, auth, course_subject='', catalog_number='', title_keyword=''):
+def search_classes(session, auth, course_subject='', catalog_number='', title_keyword=''):
+    '''
+    Searches for classes given some criteria.
+    Logs user in if not already logged in.
+    Returns list of classes if everything works, otherwise returns empty list.
+    '''
     try:
         response = session.get(search_page)
         if not logged_in(response.text):
@@ -77,6 +85,10 @@ def get_classes(session, auth, course_subject='', catalog_number='', title_keywo
 
 
 def query_page(session, query):
+    '''
+    Constructs and sends the POST request used to query the server.
+    Returns response.
+    '''
     payload = {
         'ICAction':'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH',
         'SSR_CLSRCH_WRK_SUBJECT$0':query["course_subject"],
@@ -90,7 +102,13 @@ def query_page(session, query):
 
 
 def parse_page(page):
-    write_page('temp/' + str(uuid.uuid4())[:5] + '.html', page)
+    '''
+    Parses raw XML response page.
+    Returns list of classes if the page is parsable.
+    Otherwise returns empty list.
+    '''
+    # uncomment to write every incoming page to a random file in temp/
+    #write_page('temp/' + str(uuid.uuid4())[:5] + '.html', page)
     #log('Parsing page')
     if "The search returns no results that match the criteria specified." in page:
         log('No search results match criteria')
@@ -111,10 +129,11 @@ def parse_page(page):
 
 def parse_item(item):
     '''
-    Items come in formatted as
-    [Name - Title, Catalog_number, Session, Days & Times, Room(Capacity), Instructor, Dates, Enrollment Cap, Enrolled, Fee, Select Class]
+    Parses individual table items which come in formatted as:
+    [Name - Title, Catalog_number, Session, Days & Times, Room(Capacity),
+                Instructor, Dates, Enrollment Cap, Enrolled, Fee, Select Class]
     They are returned as
-    [Status, Name, Title, Catalog Number, Times, Room, Instructor, Dates, Cap, Enrolled]
+    [Status, Name, Title, Catalog #, Times, Room, Instructor, Dates, Cap, Enrolled]
     '''
     cls = []
     if item:
@@ -129,16 +148,18 @@ def parse_item(item):
     return cls
 
 
-def output(classes, disp_arr):
-    width =  sum(disp_arr) + 3*len([x for x in disp_arr if x]) - 1
+def output(classes, display_array):
+    '''Writes classes in a pretty box given a list of classes and the display array'''
+    width =  sum(display_array) + 3*len([x for x in display_array if x]) - 1
     print('-'*width)
     if classes:
         open_classes = [x for x in classes if x[0] == "Open"]
-        format_line(['Status', 'Name', 'Title', 'Cat #', 'Times', 'Room', 'Instructor', 'Dates', 'Enr Cap', 'Enr Tot'], disp_arr)
+        format_line(['Status', 'Name', 'Title', 'Cat #', 'Times', 'Room',
+                        'Instructor', 'Dates', 'Enr Cap', 'Enr Tot'], display_array)
         print('-'*width)
         try:
             for line in classes:
-                format_line(line, disp_arr)
+                format_line(line, display_array)
         except Exception as e:
             log(str(e))
             log("Error in error print")
@@ -150,23 +171,26 @@ def output(classes, disp_arr):
     print('-'*width)
 
 
-def format_line(line, disp_arr):
+def format_line(line, display_array):
+    '''Prints and formats a single line for output'''
     for i in range(len(line)):
-        if disp_arr[i] != 0:
-           if disp_arr[i] < len(line[i]):
-               print(line[i][0:disp_arr[i]], end=' | ')
+        if display_array[i] != 0:
+           if display_array[i] < len(line[i]):
+               print(line[i][0:display_array[i]], end=' | ')
            else:
-               print(line[i].ljust(disp_arr[i]), end=' | ')
+               print(line[i].ljust(display_array[i]), end=' | ')
     print()
 
 
 def write_page(file_path, response):
+    '''Write raw text response to file at file_path'''
     log("Writing page to " + file_path)
     with open(file_path, 'w') as file:
         file.write(response)
 
 
 def write_csv(file_path, classes):
+    '''Write class list classes to csv file at file_path'''
     log("Writing classes to " + file_path)
     with open(file_path, 'w') as f:
         writer = csv.writer(f)
@@ -174,11 +198,15 @@ def write_csv(file_path, classes):
 
 
 def check_classes(session, auth, class_list):
+    '''
+    Checks if a list of classes given by class_list are available on SIS
+    Returns list of classes currently available on SIS
+    '''
     subjects = {cls[:4] : [y[5:] for y in class_list if y[:4] == cls[:4]] for cls in class_list}
     available_classes = []
     for subject in subjects:
         log('Looking for ' + subject)
-        classes = get_classes(session, authentication, course_subject=subject)
+        classes = search_classes(session, authentication, course_subject=subject)
         if not classes:
             log('No results')
         else:
@@ -190,12 +218,14 @@ def check_classes(session, auth, class_list):
 
 
 def monitor_classes(session, auth, class_list):
+    '''
+    Periodically checks if a list of classes given by class_list are available on SIS
+    Prints new classes as they become available
+    '''
     working_classes = OrderedDict()
     try:
         while True:
-
             classes = check_classes(session, auth, class_list)
-
             if classes:
                 new_classes = []
                 for cls in classes:
@@ -203,11 +233,11 @@ def monitor_classes(session, auth, class_list):
                         working_classes[cls[7]] = cls
                         new_classes.append(cls)
                 if(new_classes):
-                    output(new_classes, disp_arr)
+                    output(new_classes, display_array)
                 log("New Classes: " + str(len(new_classes)))
                 log("Old Classes: " + str(len(working_classes) - len(new_classes)))
 
-                #output([x for x in working_classes.values()], disp_arr)
+                #output([x for x in working_classes.values()], display_array)
                 time.sleep(seconds_between_requests)
     except KeyboardInterrupt:
         print()
@@ -228,8 +258,8 @@ def main():
     session = requests.session()
 
     classes = check_classes(session, authentication, class_list)
-    write_csv('temp/EECS_classes.csv', classes)
-    output(classes, disp_arr)
+    write_csv('temp/classes.csv', classes)
+    output(classes, display_array)
 
 
 if __name__ == '__main__':
